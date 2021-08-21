faaae2a8412bcb0acb1b435353fc90231c0c49e9
Taylor Council <TCouncil@willowinc.com>
2021-08-21 15:05:27 -0700

    Added mdx for md support

---
+++ components/TailwindButton.jsx
@@ -0,0 +1,9 @@
+function TailWindButton() {
+  return (
+    <button class="bg-purple-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
+  );
+}
+
+export default TailWindButton;
--- pages/index.js
+++
@@ -1,9 +0,0 @@
-function HomePage() {
-  return (
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Button
-    </button>
-  );
-}
-
-export default HomePage;
