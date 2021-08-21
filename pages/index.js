34ad74330fb159f36c97ab9d19ba79dfdacea430
Taylor Council <TCouncil@willowinc.com>
2021-08-21 14:45:52 -0700

    Added Next and Tailwind config

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
+  );
+}
+
+export default HomePage;
