f64b2bd1324a9078dec6a99b93c8dce1398b4a2a
Taylor Council <TCouncil@willowinc.com>
2021-08-22 12:33:03 -0700

    Added home to nav

--- components/layout.jsx
+++ components/layout.jsx
@@ -3,12 +3,17 @@ import Link from "next/link";
 function Layout({ children }) {
   return (
     <div class="flex flex-col w-full items-center">
-      <nav class="flex flex-row justify-between p-4 bg-gradient-to-r from-green-400 to-blue-500  w-full">
+      <nav class="flex flex-row justify-between items-center p-4 bg-gradient-to-r from-green-400 to-blue-500  w-full">
         <Hero height={50} width={50} />
-        <ul>
+        <ul class="flex flex-row space-x-2">
           <li>
             <Link href="/aboutme">
               <a>About Me</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>Home</a>
             </Link>
           </li>
         </ul>
